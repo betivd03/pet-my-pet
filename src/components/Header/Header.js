1a7a70ee1091966b809ee0a2f8be249faf4adc45
Beatris Borislavova <betivd03@abv.bg>
2021-11-28 21:26:36 +0200

    Add user to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = ({
     isAuthenticated,
-    email
+    user
 }) => {
     let guestNavigation = (
         <div id="guest">
@@ -13,7 +13,7 @@ const Header = ({
 
     let userNavigation = (
         <div id="user">
-            <span>Welcome, {email}</span>
+            <span>Welcome, {user}</span>
             <Link to="/my-pets" className="button">My Pets</Link>
             <Link to="/create" className="button">Add Pet</Link>
             <Link to="#" className="button">Logout</Link>
